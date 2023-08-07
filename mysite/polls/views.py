3c5689de51630fcf3363f662386d911472612d89
KimJungIn <rlawjddla0203@gmail.com>
2023-08-07 18:39:05 +0900

    render 사용해보기

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,28 @@
 from django.http import HttpResponse
-# Create your views here.
+from django.template import loader
+from django.shortcuts import render
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # 하드코딩은 이제 그만! .....
+
+    # return HttpResponse("Hello, world. You're at the polls index.")
+
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # output = ", ".join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # template = loader.get_template("polls/index.html")
+    # context = {
+    #     "latest_question_list": latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s" % question_id)
