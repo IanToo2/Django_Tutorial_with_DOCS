939aeb739c71677cae5b098d02cf6d38802b710d
KimJungIn <rlawjddla0203@gmail.com>
2023-08-13 17:39:49 +0900

    http 404 에러 발생시키기

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 from django.shortcuts import render
 
@@ -25,7 +25,15 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s" % question_id)
+    # return HttpResponse("You're looking at question %s" % question_id)
+    
+    # Question의 id가 없는 경우 예외 발생
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
